de6784f0ea26359781c3f41c51b999f691fc08f7
Stuart Nelson <stuart.blake.nelson@gmail.com>
2016-08-17 17:48:58 -0600

    basic segment number and segment length calculations

--- Segmenter/ViewController.swift
+++ Segmenter/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentCountLabel: UILabel!
+    @IBOutlet weak var segmentCountSlider: UISlider!
+    @IBOutlet weak var cutAngleLabel: UILabel!
+    @IBOutlet weak var outsideLengthLabel: UILabel!
+    @IBOutlet weak var outsideLengthSlider: UISlider!
+    @IBOutlet weak var diameterLabel: UILabel!
+    
+    var outsideCutLength : Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let val : Float = nextMult(outsideLengthSlider.value, Measurement.sixteenth.rawValue)
+        outsideCutLength = val
+        
+        setSegmentLabel(getCurrentSegmentCount())
+        setCutAngleLabel()
+        setOutsideLengthLabel()
+        setDiameterLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sementCountSliderAction(_ sender: UISlider) {
+        // Truncate number to int
+        let trunc : Int = Int(sender.value)
+        setSegmentLabel(trunc)
+        setCutAngleLabel()
+        setOutsideLengthLabel()
+        setDiameterLabel()
+    }
+    
+    @IBAction func outsideLengthSliderAction(_ sender: UISlider) {
+        let val : Float = nextMult(sender.value, Measurement.sixteenth.rawValue)
+        outsideCutLength = val
+        setOutsideLengthLabel()
+        setDiameterLabel()
+    }
+    
+    func setSegmentLabel(_ count : Int) {
+        let segString : String = "Segments: \(count)"
+        segmentCountLabel.text = segString
+    }
+    func getCurrentSegmentCount() -> Int {
+        return Int(segmentCountSlider.value)
+    }
+    func setCutAngleLabel() {
+        let angle : Float = Float(180/Float(Int(segmentCountSlider.value)))
+        let cutString : String = "Cut Angle: \(angle)"
+        cutAngleLabel.text = cutString
+    }
+    func setOutsideLengthLabel() {
+        let lengthString : String = "Outside Segment Length: \(outsideCutLength)"
+        outsideLengthLabel.text = lengthString
+    }
+    func setDiameterLabel() {
+        var diameter : Float = (Float(Int(segmentCountSlider.value)) * outsideCutLength) / Float(M_PI)
+        diameter = nextMult(diameter, Measurement.sixteenth.rawValue)
+        let diameterString : String = "Diameter: \(diameter)"
+        diameterLabel.text = diameterString
+    }
+    
+    func nextMult(_ num : Float, _ res : Float) -> Float {
+        return ceilf(num/res) * res
+    }
 }
 
---
+++ Segmenter/imperial.swift
@@ -0,0 +1,29 @@
+//
+//  imperial.swift
+//  Segmenter
+//
+//  Created by Stuart Nelson on 8/17/16.
+//  Copyright © 2016 Stuart Nelson. All rights reserved.
+//
+
+import Foundation
+
+
+enum Measurement : Float {
+    case sixteenth = 0.0625
+    case eighth = 0.125
+    case threeSixteenths = 0.1875
+    case fourth = 0.25
+    case fivesixteenths =  0.3125
+    case threeeights = 0.375
+    case sevensexteenths = 0.4375
+    case half = 0.5
+    case ninesexteenths = 0.5625
+    case fiveeights = 0.625
+    case elevensixteenths = 0.6875
+    case threefourths = 0.75
+    case thirteensixteenths = 0.8125
+    case seveneights = 0.875
+    case fifteensixteenths = 0.9375
+    case one = 1.0
+}
